be1e2eb544fb0d155e347c1852238adf7f3e1b75
MildlyBrain <maethaphan.phai@gmail.com>
2025-04-05 22:58:26 +0700

    feat: add server.js

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const connectDB = require("./config/db");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const { xss } = require("express-xss-sanitizer");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
+
+// route files
+const restaurants = require("./routes/restaurants");
+const reservations = require("./routes/reservations");
+const review = require("./routes/reviews");
+const auth = require("./routes/auth");
+
+dotenv.config({ path: "./config/config.env" });
+
+connectDB();
+
+// express
+const app = express();
+app.use(express.json());
+
+// libraries
+app.use(cookieParser());
+app.use(mongoSanitize());
+app.use(helmet());
+app.use(xss());
+app.use(hpp());
+app.use(cors());
+
+// use routes
+app.use("/api/restaurants", restaurants);
+app.use("/api/reservatins", reservations);
+app.use("/api/review", review);
+app.use("/api/auth", auth);
+
+// rate limiting
+const limiter = rateLimit({
+  windowsMs: 10 * 60 * 1000, // 10 minutes
+  max: 100,
+});
+app.use(limiter);
+
+// configs
+const PORT = process.env.PORT || 5000;
+const server = app.listen(
+  PORT,
+  console.log("Server running in", process.env.NODE_ENV, " mode on port ", PORT)
+);
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
+});
+
+module.exports = app;

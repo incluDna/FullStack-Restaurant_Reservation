1b37387175ee9b26f11d112e8496f42eb1dcfcf3
ShaneChaneShane <wannasut.chane@gmail.com>
2025-04-06 10:00:25 +0700

    set up folder structure

---
+++ src/app/restaurants/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function restaurantsCatalog() {
+
+  return (
+    <main>
+      <h1>view all restaurant page.. see stars, restaurant info</h1>
+    </main>
+  );
+}
+

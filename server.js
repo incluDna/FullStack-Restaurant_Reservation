ae086b05e7d0dd7c7226d1ad1e57009d9e7354bd
MildlyBrain <maethaphan.phai@gmail.com>
2025-04-07 10:48:54 +0700

    fix: change express version to 4.21.2

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.use(limiter);
 const PORT = process.env.PORT || 5000;
 const server = app.listen(
   PORT,
-  console.log("Server running in", process.env.NODE_ENV, " mode on port ", PORT)
+  console.log("Server running in", process.env.NODE_ENV, "mode on port", PORT)
 );
 
 process.on("unhandledRejection", (err, promise) => {

b3b6e31c708e2def0250c9fa9b03d9b9ccee1a08
Chanudom Tassanawitoonkrit <deedome23@gmail.com>
2025-04-11 22:47:18 +0700

    postcss.config and tailwind.config

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  content: [
+
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}

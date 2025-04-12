80c5c1b2c57c72a2ddb0dc5de1fd264b4366b00b
MildlyBrain <maethaphan.phai@gmail.com>
2025-04-12 15:43:45 +0700

    feat: Menu model

---
+++ models/Menu.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const MenuSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  restaurant: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Restaurant",
+    required: true,
+  },
+  picture: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: ["generic", "dish", "drink", "set", "dessert"],
+    required: true,
+    default: "generic",
+  },
+  description: {
+    type: String,
+    required: true,
+    maxLength: 300,
+  },
+  tag: {
+    type: String,
+    enum: [
+      "Spicy",
+      "Vegan",
+      "Gluten-free",
+      "Dairy-free",
+      "Nut-free",
+      "Halal",
+      "Locally-sourced",
+      "Signature-dish",
+      "Seasonal",
+      "Sustainable",
+      "Vegetarian",
+      null,
+    ],
+    default: null,
+  },
+});
+
+module.exports = mongoose.model("Menu", MenuSchema);

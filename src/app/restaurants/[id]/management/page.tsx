85b1434abe2f484b749b55b85aca5ee1510eaa1d
ShaneChaneShane <wannasut.chane@gmail.com>
2025-04-14 00:25:54 +0700

    feat: dynamic restaurant's reservations info page

--- src/app/restaurants/[id]/management/page.tsx
+++ src/app/restaurants/[id]/management/page.tsx
@@ -1,4 +1,4 @@
-import ReserveManageCard from "@/components/card/managementComp/ReserveManageCard"
+import ReserveManageSection from "@/components/ReserveManageSection";
 import { cookies } from "next/headers";
 
 export default async function restaurant({ params }: { params: { id: string } }) {
@@ -6,18 +6,14 @@ export default async function restaurant({ params }: { params: { id: string } })
   const role = await cookie.get('role')?.value; // check if they are not customer
   const pr = await params; // params should be awaited
   console.log(pr.id); // id for get reservation of a restaurant
-  const date = new Date().toString();
+  // const date = new Date().toString();
+
+
   return (
     <div className="w-full h-full flex flex-row font-sans">
       <div className="w-1/2 h-full bg-blue-100" />
-      <div className="w-1/2 h-full bg-white p-5 flex flex-col gap-y-5">
-        <div className="w-full h-3/2 text-left break-words text-xl">
-          Date: {date}
-        </div>
-        <ReserveManageCard />
-        <ReserveManageCard />
-        <ReserveManageCard />
-      </div>
+      <ReserveManageSection token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3ZmExZjBhNGYyMWQyZGZkZGMxZjdiMiIsImlhdCI6MTc0NDU1NTIwNCwiZXhwIjoxNzQ3MTQ3MjA0fQ.TEBX0NyXPP4sSoNfselUcaonxpDUMibQNVfKRTxhL_4" 
+      restaurantID={pr.id}/>
     </div>
   );
 }

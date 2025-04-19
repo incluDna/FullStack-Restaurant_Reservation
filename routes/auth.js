4f58fc68c39c7eecb199ce189f887c853c0199f9
ShaneChaneShane <wannasut.chane@gmail.com>
2025-04-19 18:53:17 +0700

    feat: update user's profile

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { register, login, getMe, logout } = require("../controllers/auth");
+const { register, login, getMe, logout, updateUser } = require("../controllers/auth");
 
 const router = express.Router();
 
@@ -7,7 +7,10 @@ const { protect } = require("../middleware/auth");
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/me", protect, getMe);
+router.route("/me")
+      .get(protect, getMe)
+      .put(protect, updateUser);
+
 router.get("/logout", logout);
 
 module.exports = router;

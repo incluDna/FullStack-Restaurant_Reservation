ba77b17647368f265556a05847422c8c2751d046
ShaneChaneShane <wannasut.chane@gmail.com>
2025-04-21 09:56:33 +0700

    fix: employee create menu

--- src/app/restaurants/[id]/page.tsx
+++ src/app/restaurants/[id]/page.tsx
@@ -525,7 +525,8 @@ export default function RestaurantInfo() {
         <section className="flex flex-col gap-3 px-4 lg:px-20 pb-12">
         <div className="flex flex-row justify-center items-center gap-x-4 mb-8">
           <h1 className="text-3xl font-bold text-center">Menu</h1>
-          {profile?.data?.role === "admin" && (
+          {(profile?.data?.role === "admin" ||
+          (profile?.data?.role === "employee")) && (
             <motion.button
               whileHover={{ backgroundColor: "black", scale: 1.02 }}
               transition={{ duration: 0.3 }}

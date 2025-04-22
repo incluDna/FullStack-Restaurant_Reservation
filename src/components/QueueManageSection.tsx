a556b92282dea6d8aa8cb7d33b41998d9ea2ebbd
ShaneChaneShane <wannasut.chane@gmail.com>
2025-04-22 23:13:06 +0700

    feat: queue section in management ui/logic

---
+++ src/components/QueueManageSection.tsx
@@ -0,0 +1,131 @@
+"use client"
+
+import { useEffect, useState } from "react";
+import { Queue } from "../../interfaces";
+// import getQueuesByRestaurant from "@/libs/Reservation/getQueuesByRestaurant";
+import QueueManageCard from "./card/managementComp/QueueManageCard";
+
+export default function QueueManageSection({ token, restaurantID }: { token?: string, restaurantID?: string }) {
+    const [queues, setQueues] = useState<Queue[] | undefined>(
+        undefined,
+    );
+    const [callingQueues, setCallingQueues] = useState<Queue[] | undefined>(
+        undefined,
+    );
+    const [waitingQueues, setWaitingQueues] = useState<Queue[] | undefined>(
+        undefined,
+    );
+    const [isLoading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+    // force to fetch
+    const [pleaseReload, setPleaseReload] = useState<boolean>(false);
+    // this was used to force reload component when queue card appearance should be changed/updated
+
+    if (restaurantID == null) return (
+        <div className="w-1/2 h-full text-center break-words text-xl font-bold">
+            Invalid Restaurant ID
+        </div>
+    );
+    if (token == null) return (
+        <div className="w-1/2 h-full text-center break-words text-xl font-bold">
+            Please log in first
+        </div>
+    );
+
+
+    useEffect(() => {
+        const fetchQueues = async () => {
+            try {
+                // IMPLEMENT AFTER HAVE LIBS
+                // fetching
+                // const response: ReservationJSON = await getQueuesByRestaurant(token, restaurantID);
+                // setQueues(response.data);
+                
+                // MOCK QUEUES
+                setQueues([
+                    {
+                        restaurant: '6803c841eaa0dff6a1c419e5', // sushiro
+                        user: '67fd5ab82306dfd76d6504c3', // jane
+                        seatCount: 2,
+                        createdAt: new Date(),
+                        status: 'calling',
+                    },
+                    {
+                        restaurant: '6803c841eaa0dff6a1c419e5', // sushiro
+                        user: '67f8cfa54f9feca57539a7bd', // rinuser
+                        seatCount: 2,
+                        createdAt: new Date(),
+                        status: 'waiting',
+                    }
+                ]);
+
+                console.log('test1', queues);
+            } catch (error) {
+                console.error("Error fetching queues:", error);
+                setError("Error fetching queues.");
+                setLoading(false);
+            }
+        };
+
+        fetchQueues();
+    }, [pleaseReload]);
+
+    // need separate fetch and filter useEffect cuz it's procederal otherwise it's buggy
+    useEffect(() => {
+        const filteredQueues = async () => {
+            try {
+
+                console.log('test1', queues);
+                // filtering
+                const waitingQueuesFiltered = queues?.filter((item) => item.status === 'waiting') || [];
+                const callingQueuesFiltered = queues?.filter((item) => item.status === 'calling') || [];
+                
+                setWaitingQueues(waitingQueuesFiltered);
+                setCallingQueues(callingQueuesFiltered);
+                setLoading(false);
+                console.log('test', queues);
+            } catch (error) {
+                console.error("Error fetching queues:", error);
+                setError("Error fetching queues.");
+                setLoading(false);
+            }
+        };
+
+        filteredQueues();
+    }, [queues]);
+    if (error) {
+        return <p>{error}</p>;
+    }
+
+    return (
+        <div className="w-1/2 min-h-full bg-blue-100 p-5 flex flex-col gap-y-5 items-center">
+            <div className="w-full h-fit text-center break-words text-xl font-bold">
+                Queue
+            </div>
+            <div className="w-full h-fit text-left break-words text-xl font-bold">
+                Remaing : {queues?.length||0}
+            </div>
+            <div className="w-full h-fit text-left break-words text-xl font-bold">
+                calling..
+            </div>
+            {
+                callingQueues?.map((queue, index) =>
+                (
+                    <QueueManageCard queue={queue} key={index}/>
+                )
+                )
+            }
+            <div className="w-full h-3/2 text-left break-words text-xl font-bold">
+                waiting..
+            </div>
+            {
+                waitingQueues?.map((queue, index) =>
+                (
+                    <QueueManageCard queue={queue} key={index}/>
+                )
+                )
+            }
+        </div>
+    );
+}
+

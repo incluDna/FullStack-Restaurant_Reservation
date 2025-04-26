ec775ff72a9b40aef44c98417169b60de01a2462
Chayathorn Siripongpun <chayathornsiri@gmail.com>
2025-04-26 14:45:14 +0700

    feat: update route

--- src/app/api/queue/updateQueueStatus/route.ts
+++ src/app/api/queue/updateQueueStatus/route.ts
@@ -1,14 +1,20 @@
 import { updateNotification } from "@/redux/notificationSlice";
 import { store } from "../../../../redux/store"
 import { NextApiRequest, NextApiResponse } from "next";
+import getQueuePosition from "@/libs/Queue/getQueuePosition";
+import { getAuthCookie } from "@/libs/User/getAuthCookie";
 
-const checkQueueStatus = () => {
+const checkQueueStatus = async () => {
     try {
+        // get token
+        const { success, token: newToken, role, error, profile } = await getAuthCookie();
+        if (!success) throw new Error(error || "Auth failed");
+
         // get queue by token
-        
+        const queueResponse = await 
 
         // call API, using queueId and restaurantId from queue
-        const response = { num: 2, sta: "waiting" };
+        const response = await getQueuePosition(newToken, );
         
         return response;
     } catch (error) {

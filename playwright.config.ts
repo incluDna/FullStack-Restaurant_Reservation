0215a82af5f36589c0a185718f37a36ead5dd252
Atipat Buranavatanachoke <101798829+atipata@users.noreply.github.com>
2025-04-27 14:36:06 +0700

    feat:Test Epic5-Restaurant-menu
    1.npm install
    2.npx playwright test user.test.ts employee.test.ts
    3.npx playwright show-report
    * DONT change menu at Sushiro  nakub owo*
    --------
    for customer:
     beauuser@gmail.com
     password 123456
    for sushiro employee:
     beauemployee@gmail.com
     password:123456

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,14 +13,14 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
-  /* Run tests in files in parallel */
+  /* Run tests in files in parallel -> default:false*/
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  /* Opt out of parallel tests on CI. -> default:process.env.CI ? 1 : undefined*/
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -36,18 +36,23 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { 
+        ...devices['Desktop Chrome'],
+        headless:false,
+        viewport:{width:1280, height:720},
+        launchOptions:{slowMo:1000},
+      },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {

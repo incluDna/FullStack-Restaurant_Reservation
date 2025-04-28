272ecf9e4db44e6f72e9622e95cd9237212b5caf
Atipat Buranavatanachoke <101798829+atipata@users.noreply.github.com>
2025-04-29 00:05:11 +0700

    fix: double click

--- tests/anonymous.test.ts
+++ tests/anonymous.test.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+test.setTimeout(0);  // Disable timeout for this specific test
 const FRONTEND_URL='http://localhost:3000/';
 
 test('test', async ({ page }) => {
@@ -7,7 +8,7 @@ test('test', async ({ page }) => {
   await expect(page.locator('body')).toContainText('Browse Restaurants');
   await page.locator('div').filter({ hasText: 'Welcome toSCAM !Browse' }).getByRole('button').click();
   await page.waitForTimeout(2000); // wait for 2 sec for loading page
-  await page.getByRole('button', { name: 'See More' }).dblclick();
+  await page.getByRole('button', { name: 'See More' }).click();
   await page.waitForTimeout(2000); // wait for 2 sec for loading page
   await page.locator('div:nth-child(9) > div:nth-child(2) > div:nth-child(2) > .w-full').click();
   await expect(page.getByText('MenudishdrinksetRed Shrimp w')).toBeVisible();
